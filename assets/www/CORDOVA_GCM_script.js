1a8734ad8a959248ce9eb28be45ecae4e614b204
Mark Nutter <marknutter@gmail.com>
2013-02-11 14:44:02 -0600

    fixed register call

--- assets/www/CORDOVA_GCM_script.js
+++ assets/www/CORDOVA_GCM_script.js
@@ -33,7 +33,7 @@ document.addEventListener('deviceready', function() {
   // CHANGE: your_app_id
   // TO: what ever your GCM authorized senderId is
   //
-  window.GCM.register("your_sender_id", "GCM_Event", GCM_Success, GCM_Fail );
+  window.plugins.GCM.register("283007905632", "GCM_Event", GCM_Success, GCM_Fail );
 
 }, false );
 
