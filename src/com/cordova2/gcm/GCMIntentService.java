5233d117e426208dc1b2261f0a2e355b0915448b
W Yoon <twopir99@gmail.com>
2013-05-17 06:40:44 -0400

    New version of ClientSide including push java source

--- src/com/cordova2/gcm/GCMIntentService.java
+++ src/com/cordova2/gcm/GCMIntentService.java
@@ -1,11 +1,18 @@
 package com.cordova2.gcm; //Edit this to match the name of your application
 
+import java.util.Iterator;
+
 import com.google.android.gcm.*;
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Vibrator;
 import android.util.Log;
 import com.plugin.GCM.GCMPlugin;
 
@@ -13,11 +20,40 @@ import com.plugin.GCM.GCMPlugin;
 public class GCMIntentService extends GCMBaseIntentService {
 
   public static final String ME="GCMReceiver";
+  static String re_message=null;
 
   public GCMIntentService() {
     super("GCMIntentService");
   }
   private static final String TAG = "GCMIntentService";
+  
+  /*
+  private static void generateNotification(Context context, String message) {
+	  int icon = R.drawable.ic_action_search;
+	  long when = System.currentTimeMillis();
+	
+	  NotificationManager notificationManager = (NotificationManager) context
+	    .getSystemService(Context.NOTIFICATION_SERVICE);
+	  //Notification notification = new Notification(icon, message, when);
+	  Notification notification = new Notification.Builder(context)
+	  								.setSmallIcon(icon)
+	  								.setTicker(message)
+	  								.setWhen(when)
+	  								.build();
+
+	  String title = context.getString(R.string.app_name);
+	  Intent notificationIntent = new Intent(context, MainActivity.class);
+	  re_message=message;
+
+	  notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+	  PendingIntent intent = PendingIntent.getActivity(context, 0,notificationIntent, 0);
+
+	  notification.setLatestEventInfo(context, title, message, intent);
+	  notification.flags |= Notification.FLAG_AUTO_CANCEL;
+	  notificationManager.notify(0, notification);
+  }
+  */
+  
 
   @Override
   public void onRegistered(Context context, String regId) {
@@ -54,13 +90,26 @@ public class GCMIntentService extends GCMBaseIntentService {
   @Override
   protected void onMessage(Context context, Intent intent) {
     Log.d(TAG, "onMessage - context: " + context);
+    
+    Vibrator vi = (Vibrator)getSystemService(Context.VIBRATOR_SERVICE);
+    vi.vibrate(500);
 
     // Extract the payload from the message
     Bundle extras = intent.getExtras();
     if (extras != null) {
       try
       {
-        Log.v(ME + ":onMessage extras ", extras.getString("message"));
+        Log.v(ME + ":onMessage extras ", "message : "+extras.getString("message"));
+        
+        Iterator <String> iterator = extras.keySet().iterator();
+        while( iterator.hasNext()){
+        	String key = iterator.next();
+        	String value = extras.get(key).toString();
+        	if(key.equals("msg")){
+        		setNotification(context, key, value);
+        	}
+        	Log.d("PRINT", " KEY :: " + key + " :: VALUE :: " + value);
+        }
 
         JSONObject json;
         json = new JSONObject().put("event", "message");
@@ -91,7 +140,26 @@ public class GCMIntentService extends GCMBaseIntentService {
     Log.e(TAG, "onError - errorId: " + errorId);
   }
 
-
+	  private void setNotification(Context context, String title, String message) {
+			NotificationManager notificationManager = null;
+			Notification notification = null;
+			try {
+				notificationManager = (NotificationManager) context
+						.getSystemService(Context.NOTIFICATION_SERVICE);
+				notification = new Notification(R.drawable.ic_launcher,
+						message, System.currentTimeMillis());
+				Intent intent = new Intent(context, MainActivity.class);
+				PendingIntent pi = PendingIntent.getActivity(context, 0, intent, 0);
+				// replace the title
+				title = "rematch";
+				
+				notification.setLatestEventInfo(context, title, message, pi);
+				notification.flags |= Notification.FLAG_AUTO_CANCEL;
+				notificationManager.notify(0, notification);
+			} catch (Exception e) {
+				Log.e(ME, "[setNotification] Exception : " + e.getMessage());
+			}
+		}
 
 
 }
--- src/com/cordova2/gcm/MainActivity.java
+++ src/com/cordova2/gcm/MainActivity.java
@@ -1,18 +1,128 @@
 package com.cordova2.gcm;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.app.ProgressDialog;
+import android.util.Log;
 import android.view.Menu;
+import android.webkit.CookieManager;
 
 import org.apache.cordova.*;
 
-public class MainActivity extends DroidGap {
+import com.google.android.gcm.GCMRegistrar;
 
-    @Override
+public class MainActivity extends DroidGap {
+	
+    final static String SENDER_ID = "19793081062"; // GCM Project_number
+    final static String SERVER_API_KEY = "AIzaSyC7Vo9b1OK7IMGJrHcv1agtjoEc1OlnbKE"; // Server API KEY of API_ACCESS Page  
+    static final Handler mHandler = new Handler();
+    
+    AsyncTask<String, Void, Void> regIDInsertTask;
+    ProgressDialog loagindDialog;
+    String regId ;
+    String myResult ;
+
+    @SuppressLint("NewApi")
+	@Override
     public void onCreate(Bundle savedInstanceState) {
+    	CookieManager.setAcceptFileSchemeCookies(true);
         super.onCreate(savedInstanceState);
         super.loadUrl("file:///android_asset/www/index.html");
+        
+        // GCMRegistrar Class has Client registration information 
+        // You can get client ID from here if you need
+        GCMRegistrar.checkDevice(this); 
+        GCMRegistrar.checkManifest(this); 
+        
+        regId = GCMRegistrar.getRegistrationId(this); 
+ 
+        // if Client ID is null, that means it is not registered. Thus, Try to register Server
+        if (regId.equals("") || regId==null) { 
+            GCMRegistrar.register(MainActivity.this, SENDER_ID); 
+        } else { 
+            Log.v("Main", "Already registered (" + regId + ")"); 
+        } 
+        sendAPIkey();
     }
+    
+    private void sendAPIkey() {
+		String  myNum="THIS IS MESSAGE FOR MATCHING";
+		Log.v("Main", "In sendAPIKey, After Registering  (" + regId + ")");
+		regIDInsertTask = new regIDInsertTask().execute(regId, myNum);
+	}
+    
+    /** Inner Class **/
+    private class regIDInsertTask extends AsyncTask<String, Void, Void> {
+		
+		@Override
+		protected void onPreExecute() {
+			super.onPreExecute();
+			loagindDialog = ProgressDialog.show(MainActivity.this, "Registering Key...",
+					"Please wait..", true, false);
+		}
+
+		@Override
+		protected Void doInBackground(String... params) {
+				HttpPostData(params[0] , params[1]);
+			return null;
+		}
+		
+		protected void onPostExecute(Void result) {
+			loagindDialog.dismiss();
+		}
+    }
+    
+    public void HttpPostData(String reg_id , String pnum) { 
+        try { 
+             URL url = new URL("http://websys1.stern.nyu.edu/websysS13/websysS1310/gcm_reg_insert.php");       // URL ¼³Á¤ 
+             HttpURLConnection http = (HttpURLConnection) url.openConnection();   //connect
+             //-------------------------- 
+             //   set transfer mode - basic setting 
+             //-------------------------- 
+             http.setDefaultUseCaches(false);                                            
+             http.setDoInput(true);                        
+             http.setDoOutput(true);                     
+             http.setRequestMethod("POST");         
+
+             http.setRequestProperty("content-type", "application/x-www-form-urlencoded"); 
+             StringBuffer buffer = new StringBuffer(); 
+             buffer.append("reg_id").append("=").append(reg_id).append("&");      //set value for PHP
+             buffer.append("pnum").append("=").append(pnum);
+            
+             OutputStreamWriter outStream = new OutputStreamWriter(http.getOutputStream(), "UTF-8"); 
+             PrintWriter writer = new PrintWriter(outStream); 
+             writer.write(buffer.toString()); 
+             writer.flush(); 
+             InputStreamReader tmp = new InputStreamReader(http.getInputStream(), "UTF-8");  
+             BufferedReader reader = new BufferedReader(tmp); 
+             StringBuilder builder = new StringBuilder(); 
+             String str; 
+             while ((str = reader.readLine()) != null) {    
+                  builder.append(str + "\n");                 
+       } 
+             
+              myResult = builder.toString();              
+            
+        } catch (MalformedURLException e) { 
+        	e.printStackTrace();
+            System.err.println(e.getMessage());
+        } catch (IOException e) { 
+        	e.printStackTrace();
+        	System.err.println(e.getMessage());
+        } // try end
+    } // HttpPostData end 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
